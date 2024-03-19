4b09122e6cd7d9246ad27b70361a158f74b2b0ac
Jexxes <maxygdell@protonmail.com>
2024-03-19 14:47:10 +0100

    Optimization
    Optimized pointcloud_map.py (around 275x faster than last iteration). pipeline in yaml-file set to depth.
    
    Co-Authored-By: skofta <35774710+skofta@users.noreply.github.com>

--- Software/TurtleBot4/road_navigate/road_navigate/pointcloud_map.py
+++ Software/TurtleBot4/road_navigate/road_navigate/pointcloud_map.py
@@ -1,11 +1,42 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import PointCloud2
+from sensor_msgs_py import point_cloud2
 import numpy as np
 import cv2
 import struct
 import math
 from nav_msgs.msg import OccupancyGrid
+import time
+from numba import jit
+
+
+@jit(nopython=True)
+def collect_points_into_grid(data_array, mapSizeWidth,mapSizeForward, grid_resolution):
+
+        grid_size_x = int(np.ceil((mapSizeWidth / grid_resolution)))
+        grid_size_y = int(np.ceil((mapSizeForward / grid_resolution)))
+        map_grid = np.zeros((grid_size_y, grid_size_x, 2))  # 2 channels: accumulated intensity and point count
+
+        xMapOffset = mapSizeWidth / 2
+
+
+        for point in data_array:  
+            x = point['x']
+            y = point['y']
+            z = point['z']
+            intensity = point['intensity']
+
+            if not (math.isnan(x) or math.isnan(y) or math.isnan(z) or math.isnan(intensity)) and \
+            0 < z < mapSizeForward and -xMapOffset < x < xMapOffset:
+
+                grid_x = int((x + xMapOffset) / grid_resolution)
+                grid_y = int(z / grid_resolution)
+
+                map_grid[grid_y, grid_x, 0] += intensity
+                map_grid[grid_y, grid_x, 1] += 1
+
+        return map_grid
 
 class PointCloudSubscriber(Node):
     def __init__(self):
@@ -20,30 +51,35 @@ class PointCloudSubscriber(Node):
             )
         )
 
-        
-        self.map_publisher = self.create_publisher(OccupancyGrid, '/myRoad', 10)
 
-        self.mapSizeForward = 2.5 #map size in meters
-        self.mapSizeWidth = 5.0
-        self.grid_resolution = 0.05  # 5cm per pixel
-        self.robot_base_frame = "base_link"
+        self.map_publisher = self.create_publisher(OccupancyGrid, '/myRoad', 1)
+
+        self.mapSizeForward = 2.0 #map size in meters
+        self.mapSizeWidth = 2.5
+        self.grid_resolution = 0.01  # 1cm per pixel
+        self.robot_base_frame = "oakd_link"
 
 
     def point_cloud_callback(self, msg):
 
-        base_grid = self.collect_points_into_grid(msg)
+        point_dtype = np.dtype([('x', 'f4'), ('y', 'f4'), ('z', 'f4'), ('intensity', 'f4')])
+        data_array = np.frombuffer(msg.data, dtype=point_dtype)
+
+        base_grid = collect_points_into_grid(data_array, self.mapSizeWidth, self.mapSizeForward, self.grid_resolution)
         
         avg_image = self.average_collected_points_in_grid(base_grid)
 
         filtered_image = self.filter_lines_ground(avg_image, 150)
-        cohesion_road = self.road_post_processing(filtered_image)
-        road = self.filter_lines_ground(cohesion_road, 170)
+        #cohesion_road = self.road_post_processing(filtered_image)
+        #road = self.filter_lines_ground(cohesion_road, 170)
 
-        self.map_publisher.publish(self.numpy_array_to_occupancy_grid(road))
+        self.map_publisher.publish(self.numpy_array_to_occupancy_grid(filtered_image))
 
 
 
     def numpy_array_to_occupancy_grid(self, array2d):
+        
+        array2d = np.rot90(array2d)
 
         occupancy_grid = OccupancyGrid()
         occupancy_grid.header.frame_id = self.robot_base_frame
@@ -51,62 +87,27 @@ class PointCloudSubscriber(Node):
         occupancy_grid.info.height = array2d.shape[0]
         occupancy_grid.info.resolution = self.grid_resolution
         occupancy_grid.info.origin.position.x = 0.0
-        occupancy_grid.info.origin.position.y = self.mapSizeWidth / 2
+        occupancy_grid.info.origin.position.y = -self.mapSizeWidth/2
         occupancy_grid.info.origin.position.z = 0.0
+   
         occupancy_grid.info.origin.orientation.x = 0.0
         occupancy_grid.info.origin.orientation.y = 0.0
         occupancy_grid.info.origin.orientation.z = 0.0
         occupancy_grid.info.origin.orientation.w = 1.0
 
         flattened_array = array2d.flatten()
-        occupancy_grid.data = []
-        for value in flattened_array:
-            if value >= 100:
-                occupancy_grid.data.append(100)  # 100 represents occupied
-            else:
-                occupancy_grid.data.append(value)    # 0 represents free space
+
+        min_val = np.min(flattened_array)
+        max_val = np.max(flattened_array)
+
+        normalized_array = ((flattened_array - min_val) / (max_val - min_val)) * 100
+
+        clamped_values = np.clip(normalized_array, 0, 100).astype(int)
+        occupancy_grid.data = clamped_values.tolist()
 
         return occupancy_grid
 
-
-
-    def collect_points_into_grid(self, msg):
-        data = msg.data
-        point_step = msg.point_step
-        x_offset = msg.fields[0].offset
-        y_offset = msg.fields[1].offset
-        z_offset = msg.fields[2].offset
-        intensity_offset = msg.fields[3].offset 
-
-        grid_size_x = int(np.ceil((self.mapSizeWidth / self.grid_resolution)))
-        grid_size_y = int(np.ceil((self.mapSizeForward / self.grid_resolution)))
-        map_grid = np.zeros((grid_size_y, grid_size_x, 2))  # 2 channels: accumulated intensity and point count
-
-        for i in range(0, len(data), point_step):
-            x = struct.unpack_from('f', data, i + x_offset)[0]
-            y = struct.unpack_from('f', data, i + y_offset)[0]
-            z = struct.unpack_from('f', data, i + z_offset)[0]
-            intensity = struct.unpack_from('f', data, i + intensity_offset)[0]
-
-            self.add_points_for_road(x, y, z, intensity, map_grid)
-
-        return map_grid
-
-
-    def add_points_for_road(self, x,y,z,intensity, map_grid):
-        
-        if not (math.isnan(x) or math.isnan(y) or math.isnan(z) or math.isnan(intensity)):
-            if(z < (self.mapSizeForward) and z > 0 and x < (self.mapSizeWidth / 2) and x > (-(self.mapSizeWidth / 2))):
-
-                grid_x = int((x + self.mapSizeWidth / 2) / self.grid_resolution)
-                grid_y = int((z) / self.grid_resolution)
-
-                # Accumulate intensity and number of points gatheter in the corresponding grid cell
-                map_grid[grid_y, grid_x, 0] += intensity
-                map_grid[grid_y, grid_x, 1] += 1
-
-
-
+    
     def average_collected_points_in_grid(self, map_grid):
         avg_image = np.zeros_like(map_grid[..., 0])
         mask = (map_grid[..., 1] != 0)
@@ -134,3 +135,5 @@ def main(args=None):
     rclpy.spin(point_cloud_subscriber)
     point_cloud_subscriber.destroy_node()
     rclpy.shutdown()
+
+
